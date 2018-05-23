b834e0c2e3ad1e7ab5082c52c69d2d44ae49a851
Peter Haley <pblhaley@gmail.com>
2018-05-23 16:36:10 -0500

    Add bootstrap, show page, and show all posts on landing page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
 
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -10,6 +14,13 @@ class PostsController < ApplicationController
 
   def create
     # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
